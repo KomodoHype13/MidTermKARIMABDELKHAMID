f1113a07cf018e296e3d545bfa1fedc42fad1525
Kerim <darckqueen144@gmail.com>
2021-04-10 10:44:15 +0300

    Question 6

--- index.php
+++ index.php
@@ -2,9 +2,27 @@
 <html>
 <body>
 
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Lisence plate: <input type="text" name="lplate">
+  Brand: <input type="text" name="brnd">
+  Model: <input type="text" name="mdl">
+  Model Year: <input type="text" name="mdlyr">
+  Color: <input type="text" name="clr">
+  <input type="submit">
+</form>
+
 <?php
 echo "welcome to the midterm exam<br>";
+include 'Automobile.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    $Lisence_plate =$_POST['lplate'];
+    $Brand =$_POST['brnd'];
+    $Model =$_POST['mdl'];
+    $Model_year =$_POST['mdlyr'];
+    $Color =$_POST['clr'];
+}
 ?>
 
 </body>
